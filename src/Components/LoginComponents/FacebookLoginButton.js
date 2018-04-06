7beee063d7ca9776cceed6f847d62d6596225378
jihdeh <jide.b.tade@gmail.com>
2018-04-06 08:55:38 +0100

    integrate facebook login (WIP)

--- src/Components/LoginComponents/FacebookLoginButton.js
+++ src/Components/LoginComponents/FacebookLoginButton.js
@@ -3,9 +3,25 @@ import { TouchableOpacity, Text, Image } from "react-native";
 import Styles from "../../Styles/FacebookLoginButtonStyles";
 
 export default class FacebookLoginButton extends Component {
+  async onClick() {
+    const { type, token } = await Expo.Facebook.logInWithReadPermissionsAsync(
+      "194433294293107",
+      {
+        permissions: ["public_profile"]
+      }
+    );
+    if (type === "success") {
+      // Get the user's name using Facebook's Graph API
+      const response = await fetch(
+        `https://graph.facebook.com/me?access_token=${token}`
+      );
+      Alert.alert("Logged in!", `Hi ${(await response.json()).name}!`);
+    }
+  }
+
   render() {
     return (
-      <TouchableOpacity style={Styles.button}>
+      <TouchableOpacity style={Styles.button} onPress={() => this.onClick()}>
         <Image
           source={require("../../../assets/Icons/facebook-icon.png")}
           style={Styles.facebookIcon}
