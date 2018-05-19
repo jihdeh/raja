2184bd0fadaf1d8fe76adf89e35075d1832fe501
jihdeh <jide.b.tade@gmail.com>
2018-05-20 00:13:22 +0100

    implement pusher

--- src/Components/Notifications.js
+++ src/Components/Notifications.js
@@ -7,21 +7,29 @@ import {
   RefreshControl,
   Image
 } from "react-native";
+import Pusher from "pusher-js/react-native";
 import GStyles from "../Styles/GeneralStyle";
 
-const dummyData = [
-  {
-    message: "Ben law bookmarked your item"
-  },
-  {
-    message: "Two of your new products have been purchased"
-  }
-];
+const socket = new Pusher("0a8c0a7646c00d9e3227", {
+  cluster: "eu"
+});
+
+const channel = socket.subscribe("auction-bid");
 
 const fetchData = () =>
   new Promise((resolve, reject) => setTimeout(() => resolve(dummyData), 2000));
 
 class Notification extends Component {
+  state = {
+    data: [
+      {
+        message: "Ben law bookmarked your item"
+      },
+      {
+        message: "Two of your new products have been purchased"
+      }
+    ]
+  };
   static navigationOptions = ({ navigation }) => {
     return {
       headerTitle: "Notifications",
@@ -42,6 +50,16 @@ class Notification extends Component {
       refreshing: false
     };
   }
+
+  componentWillMount() {
+    channel.bind("new-bid", data => {
+      console.log(data.message);
+      this.setState({
+        data: [...this.state.data, data.message]
+      });
+    });
+  }
+
   _onRefresh() {
     this.setState({ refreshing: true });
     fetchData().then(() => {
@@ -60,23 +78,29 @@ class Notification extends Component {
           />
         }
       >
-        {dummyData.map((notification, key) => {
-          return (
-            <View
-              key={key}
-              style={{
-                borderBottomWidth: 1,
-                borderBottomColor: "#d6d7d8",
-                padding: 10
-              }}
-            >
-              <Text>
-                {notification.message} -{" "}
-                <Text style={{ fontSize: 10 }}>3w ago</Text>
-              </Text>
-            </View>
-          );
-        })}
+        {data.length ? (
+          data.reverse().map((notification, key) => {
+            return (
+              <View
+                key={key}
+                style={{
+                  borderBottomWidth: 1,
+                  borderBottomColor: "#d6d7d8",
+                  padding: 10
+                }}
+              >
+                <Text>
+                  {notification.message} -{" "}
+                  <Text style={{ fontSize: 10 }}>3w ago</Text>
+                </Text>
+              </View>
+            );
+          })
+        ) : (
+          <View>
+            <Text>No new notifications</Text>
+          </View>
+        )}
       </ScrollView>
     );
   }
